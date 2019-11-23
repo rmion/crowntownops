87769c84ddddf5c6f64456617c995e1a8da43aa4
Robert Mion <mr.robert.mion@gmail.com>
2019-11-23 09:38:42 -0500

    Fixed seure protocol string

--- public/app.js
+++ public/app.js
@@ -18,7 +18,7 @@
       stops: [],
       stopsRemaining: null,
       counter: 0,
-      isSecureConnection: window.location.protocol == 'https://',
+      isSecureConnection: window.location.protocol == 'https:',
       isRouteLoading: false,
       isRouteLoaded: false,
       isDataLoaded: false,
